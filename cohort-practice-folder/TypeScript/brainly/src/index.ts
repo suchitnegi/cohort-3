58f2df17d1bc50a6b64d8a2c07bdd548412c46df
suchitnegi <suchitnegi99@gmail.com>
2025-06-24 09:07:30 +0530

    initialize ts be

---
+++ cohort-practice-folder/TypeScript/brainly/src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+import jwt from 'jsonwebtoken'
+
+const app = express();
+
+app.post("/api/v1/signup",(req,res)=>{
+})
+app.post("/api/v1/signin",(req,res)=>{
+})
+app.post("/api/v1/content",(req,res)=>{
+    
+})
+app.get("/api/v1/content",(req,res)=>{
+    
+})
+app.delete("/api/v1/content",(req,res)=>{
+    
+})
